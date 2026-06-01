63136902e7ce20e07902b021fc7d59b95eeaeecb
Silvia Trovato <strovato@hm.edu>
2026-06-01 19:12:18 +0200

    fa2 algo implementiert

--- graph_big/preprocessing/community_detection.py
+++ graph_big/preprocessing/community_detection.py
@@ -2,10 +2,13 @@ import networkx as nx
 import pandas as pd
 import community as community_louvain
 from collections import Counter
+import os
+import json
 
 # --- Graph laden  ---
-edges = pd.read_csv('data_facebook_large/musae_facebook_edges.csv')
-targets = pd.read_csv('data_facebook_large/musae_facebook_target.csv')
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+edges   = pd.read_csv(os.path.join(BASE_DIR, '../data_facebook_large/musae_facebook_edges.csv'))
+targets = pd.read_csv(os.path.join(BASE_DIR, '../data_facebook_large/musae_facebook_target.csv'))
 
 G = nx.from_pandas_edgelist(edges, source='id_1', target='id_2') # csv zu network-x-graph
 label_map = dict(zip(targets['id'], targets['page_type']))# {0: 'politician', 1: 'company', ...}
@@ -16,6 +19,8 @@ nx.set_node_attributes(G, label_map, 'category') # hängt dict als atrriubt an j
 
 print("\n=== Community Detection (Louvain) ===")
 partition = community_louvain.best_partition(G, random_state=42) #{ 0: 4, 1: 4, 2: 17, 3: 4, 5: 12, ... }: node_id → community_id
+nx.set_node_attributes(G, partition, 'community')
+print(dict(list(G.nodes(data=True))[:5]))
 
 
 
@@ -31,3 +36,9 @@ community_sizes = Counter(partition.values())
 print(f"\nGrößte Community:  {max(community_sizes.values())} Nodes")
 print(f"Kleinste Community: {min(community_sizes.values())} Nodes")
 print(f"Median Community:   {sorted(community_sizes.values())[num_communities//2]} Nodes")
+
+# --- Export ---
+communities_path = os.path.join(BASE_DIR, 'communities.json')
+with open(communities_path, 'w') as f:
+    json.dump(partition, f)
+print("communities.json gespeichert")
